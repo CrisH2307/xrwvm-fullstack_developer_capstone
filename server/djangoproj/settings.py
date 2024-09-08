a05efb23deb074bf9fe0cf2dbf716772dfa5f893
CrisH2307 <khangpicasso@gmail.com>
2024-09-08 12:43:48 -0400

    OK

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,17 +30,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'khangpicasso-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'khangpicasso-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'khangpicasso-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://khangpicasso-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'https://khangpicasso-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://khangpicasso-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://khangpicasso-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+    'https://khangpicasso-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
